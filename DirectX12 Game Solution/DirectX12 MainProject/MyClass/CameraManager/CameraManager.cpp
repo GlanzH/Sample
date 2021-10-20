6ed1dc8ef75e512c953001fbaaadfccdaa30d5b4
木幡 光秀 <takeminakatanomikoto71@gmail.com>
2021-10-20 17:55:26 +0900

    [Save]カメラ途中

--- DirectX12 Game Solution/DirectX12 MainProject/MyClass/CameraManager/CameraManager.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/MyClass/CameraManager/CameraManager.cpp
@@ -2,22 +2,17 @@
 #include "Base/dxtk.h"
 #include "CameraManager.h"
 
-CameraManager::CameraManager() {
-
-}
-
-CameraManager::~CameraManager() {
-
-}
-
 bool CameraManager::Initialize() {
+	//カメラの位置
+	camera->SetView(Vector3(0.0f, 0.0f, -10.0f),Vector3(0.0f, 0.0f, 0.0f));
+
+	//カメラの向き・映す距離
+	camera->SetPerspectiveFieldOfView(
+		XMConvertToRadians(90.0f), 16.0f / 9.0f, 1.0f, 10000.0f
+	);
 	return true;
 }
 
 int CameraManager::Update() {
 	return 0;
-}
-
-void CameraManager::Render() {
-
 }
