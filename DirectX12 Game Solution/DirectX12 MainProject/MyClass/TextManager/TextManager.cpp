5cda3565402ebc08f368618ee0f6f9847d010793
MSI\jfuki <jfukiage46@gmail.com>
2021-10-26 12:17:05 +0900

    [Update]スペースキーを押すことでテキストが書き換わるようにした [Update]テキストcsvの更新

--- DirectX12 Game Solution/DirectX12 MainProject/MyClass/TextManager/TextManager.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/MyClass/TextManager/TextManager.cpp
@@ -3,9 +3,15 @@
 #include "TextManager.h"
 
 bool TextManager::Initialize() {
-	font = DX9::SpriteFont::CreateDefaultFont(DXTK->Device9);
-	write = 0.0f;
+	font = DX9::SpriteFont::CreateFromFontName(DXTK->Device9, L"ＭＳ ゴシック", 22);
+	for (int i = 0; i < 5; ++i)
+		write[i] = 0.0f;
+
 	writeFlag = false;
+	changeFlag = false;
+	
+	read = 0.0f;
+
 	return true;
 }
 
@@ -19,15 +25,20 @@ void TextManager::LoadText() {
 
 	wchar_t read_line[256];
 
-	for (int i = 0; i < 1; ++i) {
+	for (int i = 0; i < 5; ++i) {
 		fgetws(read_line, 255, fp);
+		const int LEN = lstrlenW(read_line);
+		if (LEN <= 1)
+			continue;
+
+		read_line[LEN - 1] = '\0';
 		text[i] = read_line;
 
 		//改行を設定する
-		for (int j = 0; j < text[i].length(); ++j) {
-			if (text[i].at(j) == L'@')
-				text[i].at(j) = L'\n';
-		}
+		//for (int j = 0; j < text[i].length(); ++j) {
+		//	if (text[i].at(j) == L'@')
+		//		text[i].at(j) = L'\n';
+		//}
 	}
 	fclose(fp);	//ファイルを閉じる
 }
@@ -35,14 +46,35 @@ void TextManager::LoadText() {
 void TextManager::Update(const float deltaTime) {
 	if (DXTK->KeyEvent->pressed.Enter)
 		writeFlag = true;
+	
+	
+	read = text[0] + text[1] + text[3];
 
-	if (writeFlag == true)
-		write += 50.0f * deltaTime;
-		//write = std::min(write + 10.0f * deltaTime, text[0].length());
+	if (writeFlag == true) {
+		for (int i = 1; i < 2; ++i) {
+			if(write[i] < read.length()) {
+				write[i] += WRITE_SPEED * deltaTime;
+			}
+
+			//if (write > text[i].length())
+			//	write = text[i].length();
+		}
 	
-	if (write > text[0].length())
-		write = text[0].length();
+		if (DXTK->KeyEvent->pressed.Space) {
+			write[1] = 3.0f;
+			changeFlag = true;
+		}
+	}
+
 	
+	if (changeFlag == true) {
+		read = text[0] + text[2] + text[3];
+		write[1] += WRITE_SPEED * deltaTime;
+
+		if (write[1] > read.length()) {
+			write[1] = read.length();
+		}
+	}
 	return;
 }
 
@@ -52,6 +84,15 @@ void TextManager::Render() {
 
 void TextManager::Render2D() {
 	Rect dest = RectWH(0, 0, 1000, 1000);
-	DX9::SpriteBatch->DrawText(font.Get(), text[0].c_str(), (int)write, dest, DX9::Colors::Black);
+	DX9::SpriteBatch->DrawText(font.Get(), read.c_str(), (int)write[1], dest, DX9::Colors::Black);
+
+	//dest = RectWH(426, 0, 1000, 1000);
+	//DX9::SpriteBatch->DrawText(font.Get(), text[2].c_str(), (int)write[2], dest, DX9::Colors::Black);
+
+	//dest = RectWH(116, 0, 1000, 1000);
+	//DX9::SpriteBatch->DrawText(font.Get(), text[3].c_str(), (int)write[3], dest, DX9::Colors::Red);
+
+	//dest = RectWH(116, 0, 1000, 1000);
+	//DX9::SpriteBatch->DrawText(font.Get(), text[4].c_str(), (int)write[4], dest, DX9::Colors::Blue);
 
 }
