85298f7e793187a8a513f303bd9a31a76808b562
木幡 光秀 <takeminakatanomikoto71@gmail.com>
2021-10-27 09:23:32 +0900

    [Update]PlayerManagerのGetHitFlagによる条件文の追加

--- DirectX12 Game Solution/DirectX12 MainProject/MyClass/EnemyManager/EnemyManager.h
+++ DirectX12 Game Solution/DirectX12 MainProject/MyClass/EnemyManager/EnemyManager.h
@@ -1,6 +1,7 @@
 #pragma once
 
 using namespace DirectX;
+#include "../PlayerManager/PlayerManager.h"
 
 typedef SimpleMath::Vector3 Vector3;
 
@@ -17,11 +18,15 @@ public:
 	DX9::MODEL& GetCollision() { return collision; }
 
 private:
+	void Move(DX9::MODEL& ground, const float deltaTime);
+
 	DX9::SKINNEDMODEL model;
 	DX9::MODEL		  collision;
 	
 	BoundingBox  box;
 	D3DMATERIAL9 material;
+
+	PlayerManager player;
 
 	const Vector3 init_pos = Vector3(50.0f, 0.0f, 50.0f);
 };
