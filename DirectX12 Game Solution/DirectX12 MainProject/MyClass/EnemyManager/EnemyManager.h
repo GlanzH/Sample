5d50cd076989fae2a7c1cc944f6425e48d5a75a6
木幡 光秀 <takeminakatanomikoto71@gmail.com>
2021-10-28 23:09:02 +0900

    [Add]EnemyManagerにGetDeathEnemyCount関数追加

--- DirectX12 Game Solution/DirectX12 MainProject/MyClass/EnemyManager/EnemyManager.h
+++ DirectX12 Game Solution/DirectX12 MainProject/MyClass/EnemyManager/EnemyManager.h
@@ -17,9 +17,12 @@ public:
 	int  Update(DX9::MODEL& ground, const float deltaTime);
 	void Render();
 
+	int GetDeathEnemyCount() { return dead_enemy_count; }
+
 private:
 	std::list<EnemyBase*> enemy;
 
+	int dead_enemy_count = 0;
 
 	//	void Move(DX9::MODEL& ground, const float deltaTime);
 	//const Vector3 init_pos = Vector3(50.0f, 0.0f, 50.0f);
