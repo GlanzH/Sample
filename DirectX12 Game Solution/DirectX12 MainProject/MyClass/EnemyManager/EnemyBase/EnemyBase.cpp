613578cfa10294a6fa5cff726824cb8679d46936
木幡 光秀 <takeminakatanomikoto71@gmail.com>
2021-11-02 21:32:23 +0900

    [Update]敵モデル変更

--- DirectX12 Game Solution/DirectX12 MainProject/MyClass/EnemyManager/EnemyBase/EnemyBase.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/MyClass/EnemyManager/EnemyBase/EnemyBase.cpp
@@ -14,7 +14,6 @@ void EnemyBase::LoadAsset(LPCWSTR model_name, SimpleMath::Vector3 initial_positi
 	position = initial_position;
 
 	model = DX9::Model::CreateFromFile(DXTK->Device9, model_name);
-	//model->SetScale(5.f);
 	model->SetPosition(position);
 	model->SetRotation(0.0f, XMConvertToRadians(90.0f), 0.0f);
 
