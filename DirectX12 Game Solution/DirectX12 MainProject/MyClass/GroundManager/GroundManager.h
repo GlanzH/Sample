e30ef1a10ce570d1c5214a4c2091dcbdeda0ea02
木幡 光秀 <takeminakatanomikoto71@gmail.com>
2021-11-20 00:51:41 +0900

    [Update]劇場モデルの描画・あたり判定の設定した

--- DirectX12 Game Solution/DirectX12 MainProject/MyClass/GroundManager/GroundManager.h
+++ DirectX12 Game Solution/DirectX12 MainProject/MyClass/GroundManager/GroundManager.h
@@ -3,6 +3,8 @@
 #include "Base/pch.h"
 #include "Base/dxtk.h"
 
+using namespace DirectX;
+
 class GroundManager {
 public:
 	GroundManager() {};
@@ -11,8 +13,15 @@ public:
 	void LoadAsset();
 	void Render();
 
+	BoundingBox GetBox() { return box; }
+
 private:
 	DX9::MODEL model;
+	DX9::MODEL collision;
 
-	const float pos_z = 50.0f;
+	BoundingBox box;
+
+	const float box_size =  2.0f;
+	const float pos_y	 = 25.0f;
+	const float pos_z	 = 70.0f;
 };
