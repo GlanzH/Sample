d7746e8838014026daad990f2f44862916161f63
MSI\jfuki <jfukiage46@gmail.com>
2021-11-25 17:40:11 +0900

    [Add]タイトルシーン、リザルトシーン、タイトル・リザルトの仮画像、UI必殺技ゲージの仮画像を追加 [Update]UIManager仮画像の描画

--- DirectX12 Game Solution/DirectX12 MainProject/MyClass/UIManager/UIManager.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/MyClass/UIManager/UIManager.cpp
@@ -3,9 +3,13 @@
 void UIManager::LoadAsset() {
 	volt_empty = DX9::Sprite::CreateFromFile(DXTK->Device9, L"UI/ui_empty.png");
 	volt_max   = DX9::Sprite::CreateFromFile(DXTK->Device9, L"UI/ui_max.png"  );
+	sword_empty = DX9::Sprite::CreateFromFile(DXTK->Device9, L"UI/empty_sword.png");
+	sword_max	= DX9::Sprite::CreateFromFile(DXTK->Device9, L"UI/sword.png"	  );
 }
 
 void UIManager::Render(float voltage) {
 	DX9::SpriteBatch->DrawSimple(volt_empty.Get(), SimpleMath::Vector3(0.0f, 0.0f, 0.0f));
 	DX9::SpriteBatch->DrawSimple(volt_max.Get(), SimpleMath::Vector3(0.0f, 0.0f, 0.0f), RectWH(0, 0, (int)voltage, 86));
+	DX9::SpriteBatch->DrawSimple(volt_empty.Get(), SimpleMath::Vector3(0.0f, 0.0f, 0.0f));
+	DX9::SpriteBatch->DrawSimple(sword_max.Get(), SimpleMath::Vector3(0.0f, 0.0f, 0.0f), RectWH(95, 0, (int)32.4, 124));
 }
