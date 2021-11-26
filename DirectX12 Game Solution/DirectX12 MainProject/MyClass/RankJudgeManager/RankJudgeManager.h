d9c3e35062a8985c29c16ba2f8f8b8ac6bdb98e5
MSI\jfuki <jfukiage46@gmail.com>
2021-11-26 19:46:33 +0900

    [Add]RankJudgeManagerの追加

---
+++ DirectX12 Game Solution/DirectX12 MainProject/MyClass/RankJudgeManager/RankJudgeManager.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Base/pch.h"
+#include "cppcoro/generator.h"
+
+class RankJudgeManager {
+public:
+	RankJudgeManager() {};
+	~RankJudgeManager() {};
+
+	void Initialize();
+
+	void GetVoltage();
+
+	void JudgeRnak();
+
+private:
+	float now_score;
+	int now_rank;
+	enum RANK {
+		D,C,B,A,S
+	};
+	// コルーチンのプロトタイプ宣言
+	cppcoro::generator<int> ReleaseRank();
+	cppcoro::generator<int>                  co_routine;
+	cppcoro::detail::generator_iterator<int> co_routine_it;
+};
