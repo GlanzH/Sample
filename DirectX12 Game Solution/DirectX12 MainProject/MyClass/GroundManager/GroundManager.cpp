9ebeb659090e28be54100540063b7e6e7dd0ccdb
木幡 光秀 <takeminakatanomikoto71@gmail.com>
2021-12-04 14:33:47 +0900

    [Add]とりあえず、観客モデル追加

--- DirectX12 Game Solution/DirectX12 MainProject/MyClass/GroundManager/GroundManager.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/MyClass/GroundManager/GroundManager.cpp
@@ -1,7 +1,7 @@
 #include "GroundManager.h"
 
 void GroundManager::LoadAsset() {
-	model = DX9::Model::CreateFromFile(DXTK->Device9, L"Model//Theater\\background.x");	
+	model = DX9::Model::CreateFromFile(DXTK->Device9, L"Model\\Theater\\background.x");	
 	model->SetPosition(0.0f, pos_y, 10.0f);
 }
 
