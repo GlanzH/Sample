2f593b962b1e9470b5e4f78148227410d63ff38a
木幡 光秀 <takeminakatanomikoto71@gmail.com>
2021-12-10 14:11:02 +0900

    [Update]ラミエルの高さ調整 [Update]会話シーンをゲームパッド対応 [Fix]当たり判定復活

--- DirectX12 Game Solution/DirectX12 MainProject/MyClass/GroundManager/GroundManager.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/MyClass/GroundManager/GroundManager.cpp
@@ -2,7 +2,7 @@
 
 void GroundManager::LoadAsset() {
 	model = DX9::Model::CreateFromFile(DXTK->Device9, L"Model\\Theater\\background.X");	
-	model->SetPosition(0.0f, pos_y, 10.0f);
+	model->SetPosition(0.0f, pos_y, 20.0f);
 }
 
 void GroundManager::Render() {
