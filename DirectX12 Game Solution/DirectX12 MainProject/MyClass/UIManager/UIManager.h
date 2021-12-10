273d3ea21371de8d56cc108b13960dfcb99d843c
MSI\jfuki <jfukiage46@gmail.com>
2021-12-10 12:35:33 +0900

    [Update]ハートゲージを剣ゲージに変更。 [Add]剣ゲージを追加

--- DirectX12 Game Solution/DirectX12 MainProject/MyClass/UIManager/UIManager.h
+++ DirectX12 Game Solution/DirectX12 MainProject/MyClass/UIManager/UIManager.h
@@ -33,6 +33,6 @@ private:
 	const float HEART_UI_POS_X = 84.0f;
 	const float HEART_UI_POS_Y = 137.0f;
 	const int HEART_MAX_WIDTH = 89;
-	const int HEART_MAX_HIGH  = 75;
+	const int HEART_MAX_HIGH  = 89;
 
 };
