66befce39065bcc3d52bfe16e4c41ffdd0be7952
木幡 光秀 <takeminakatanomikoto71@gmail.com>
2021-12-10 11:45:29 +0900

    [Update]シェーダー直すのめんどいのでモデル変えた

--- DirectX12 Game Solution/DirectX12 MainProject/MyClass/GroundManager/GroundManager.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/MyClass/GroundManager/GroundManager.cpp
@@ -1,7 +1,7 @@
 #include "GroundManager.h"
 
 void GroundManager::LoadAsset() {
-	model = DX9::Model::CreateFromFile(DXTK->Device9, L"bg_forest\\bg_forest.X");	
+	model = DX9::Model::CreateFromFile(DXTK->Device9, L"Model\\Theater\\background.X");	
 	model->SetPosition(0.0f, pos_y, 10.0f);
 }
 
