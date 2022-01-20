c1c9f6e9fc0fb57887fed95cb7619823bd41b592
木幡 光秀 <takeminakatanomikoto71@gmail.com>
2022-01-20 22:52:00 +0900

    Merge branch 'master' of github.com:GlanzH/Sample into kowata_mk.2

--- DirectX12 Game Solution/DirectX12 MainProject/MyClass/EnemyManager/EnemyFactory/EnemyBase/EnemyBase.h
+++ DirectX12 Game Solution/DirectX12 MainProject/MyClass/EnemyManager/EnemyFactory/EnemyBase/EnemyBase.h
@@ -20,21 +20,21 @@ public:
 	EnemyBase() {};
 	~EnemyBase() {};
 
-	virtual bool Initialize(std::string tag, double init_wait,double stop_pos,std::string time_stop_flag,
-		                    double speed,std::string direct, std::string posture, int hp);
+	virtual bool Initialize(std::string tag, double init_wait, double stop_pos, std::string time_stop_flag,
+		double speed, std::string direct, std::string posture, int hp);
 
 	virtual void LoadAsset(LPCWSTR model_name, SimpleMath::Vector3 initial_position);
 	void LoadModel(LPCWSTR model_name, SimpleMath::Vector3 initial_position);
 
-	virtual int  Update(SimpleMath::Vector3 player,bool special_attack_flag, bool thorow_things_flag, const float deltaTime);
+	virtual int  Update(SimpleMath::Vector3 player, bool special_attack_flag, bool thorow_things_flag, const float deltaTime);
 	virtual void Render() {};
 	void Retreat();
 
 	void TemporaryDeath(float max_death);
-	bool GetTimeStopFlag()       { return do_time_stop_flag; }
+	bool GetTimeStopFlag() { return do_time_stop_flag; }
 	bool GetTemporaryDeathFlag() { return temporary_death_flag; }
-	bool GetAttackFlag()         { return attack_flag; }
-	
+	bool GetAttackFlag() { return attack_flag; }
+
 	std::string GetPostune() { return enemy_posture; }
 
 	void HitEffect();
@@ -45,17 +45,17 @@ public:
 	virtual bool LifeDeathDecision() { return LIVE; }
 
 	DX9::SKINNEDMODEL& GetAnimModel() { return anim_model; }
-	DX9::MODEL& GetModel()			  { return model; }
-	Collision GetBox()				  { return col; }
+	DX9::MODEL& GetModel() { return model; }
+	Collision GetBox() { return col; }
 
 	std::string GetTag() { return enemy_tag; }
 
 private:
 	void TimeStopDecision();
 	void IsDamage();
-	
-	EFFECTHANDLE hit_handle,star_handle,normal_die_handle,special_die_handle,love_handle;
-	EFFECT hit, star, normal_die,special_die, love;
+
+	EFFECTHANDLE hit_handle, star_handle, normal_die_handle, special_die_handle, love_handle;
+	EFFECT hit, star, normal_die, special_die, love;
 
 	ExplodeMan explode;
 
@@ -64,7 +64,7 @@ private:
 
 	std::string enemy_tag;
 
-	int   retreat_count   = 0;
+	int   retreat_count = 0;
 	const int max_retreat = 30;
 
 	float auto_destroy_frame = 0.0f;
@@ -91,13 +91,13 @@ private:
 
 	const float box_size = 2.0f;
 
-	
+
 protected:
 	virtual void Action() {}
-	virtual void Move()   {}
+	virtual void Move() {}
 	virtual void IsRetreat();
 
-	void SetAnimation(DX9::SKINNEDMODEL& model,const int enabletack,int max_motion);
+	void SetAnimation(DX9::SKINNEDMODEL& model, const int enabletack, int max_motion);
 	void AdjustAnimCollision();
 	bool Stun();
 
@@ -119,12 +119,12 @@ protected:
 	const float rotate = 45.0f;
 
 	float init_wait_frame = 0.0f;
-	float death_frame     = 0.0f;
+	float death_frame = 0.0f;
 	float max_init_wait;
 
 	std::string enemy_direct;
 	std::string enemy_posture;
-	
+
 	int    enemy_hp;
 	double move_speed;
 	double enemy_stop;
