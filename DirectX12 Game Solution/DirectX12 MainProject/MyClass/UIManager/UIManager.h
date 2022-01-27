5356aec1daff3cc610e13a77c44f76b4d565d6aa
MSI\jfuki <jfukiage46@gmail.com>
2022-01-27 11:54:36 +0900

    [Update] UIの画像更新

--- DirectX12 Game Solution/DirectX12 MainProject/MyClass/UIManager/UIManager.h
+++ DirectX12 Game Solution/DirectX12 MainProject/MyClass/UIManager/UIManager.h
@@ -92,10 +92,14 @@ private:
 	//“Gc‚è”
 	DX9::SPRITE enemy;
 	DX9::SPRITE enemy_dead;
-	int enemy_width;
-	int enemy_dead_width;
 
-	const int ENEMY_MIN_WIDTH = 42;
+	int enemy_max_num;
+	int enemy_dead_width;
+	float enemy_pos_x;
+	float enemy_pos_y;
+	bool enemy_dead_flag[100];
+
+	const float ENEMY_MIN_POS_X = 500.0f;
 	const int ENEMY_HIGHT = 42;
 
 	//2DƒJƒƒ‰
