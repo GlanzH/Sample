848bac0f5644b041c7ceed0ca375e7e7fc8834d4
木幡 光秀 <takeminakatanomikoto71@gmail.com>
2022-01-27 18:25:28 +0900

    [Fix]プレイヤー旧版になっていたのを修正

--- DirectX12 Game Solution/DirectX12 MainProject/MyClass/Observer/Observer.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/MyClass/Observer/Observer.cpp
@@ -9,14 +9,18 @@ void Observer::CollisionDetection(PlayerBase* player, EnemyManager* enemy, Audie
 	for (auto enemies_roop : enemy->GetEnemy()) {
 		if (player->IsAttack()) {
 			if (player->GetBox().sword_box.Intersects(enemies_roop->GetBox().box)) {
-				if (enemies_roop->GetPostune() == "U" && player->GetAttackTag() == 1 && enemies_roop->FrontFlag()) {
+				if (enemies_roop->GetAttackFlag() && enemies_roop->GetPostune() == "U" &&
+					player->GetAttackTag() == 1 && enemies_roop->FrontFlag()
+					) {
 					//はじかれるフラグをtrueにする関数を呼び出す
 					//trueなら弾かれた時の処理を行う関数は別に作る
 
 					player->GetFripFlag();
 
 				}
-				else if (enemies_roop->GetPostune() == "D" && player->GetAttackTag() == 2 && enemies_roop->FrontFlag()) {
+				else if (enemies_roop->GetAttackFlag() && enemies_roop->GetPostune() == "D" &&
+					player->GetAttackTag() == 2 && enemies_roop->FrontFlag()
+					) {
 					//はじかれるフラグをtrueにする関数を呼び出す
 					//trueなら弾かれた時の処理を行う関数は別に作る
 					player->GetFripFlag();
