6539aac38980181adc3e5457ff3332ea01242e26
MSI\jfuki <jfukiage46@gmail.com>
2022-01-27 18:56:47 +0900

    [Update] ウェーブの時間が10秒切ったら拡大縮小するようにした

--- DirectX12 Game Solution/DirectX12 MainProject/MyClass/WaveProcess/WaveProcess.h
+++ DirectX12 Game Solution/DirectX12 MainProject/MyClass/WaveProcess/WaveProcess.h
@@ -21,9 +21,14 @@ private:
 	int time_one_digit;	//1桁目表示変数
 	int time_two_digit;	//2桁目表示変数
 	int now_time;	//現在の時間
-
-	float one_digit_pos_x; //1桁目の場所
+	int scale_mode;	//拡大縮小モード
+	float time_num_scale;	//時間の大きさ
 	bool one_digit_flag;	//タイムが1桁になったかフラグ
+
+	enum SCALEMODE {
+		EXPAND,	//拡大
+		SHRINK	//縮小
+	};
 
 	const int TIME_WIDTH = 111;
 	const int TIME_HIGHT = 34;
@@ -33,9 +38,11 @@ private:
 
 	const float TIME_POS_X = 560.0f;
 	const float TIME_POS_Y = 20.0f;
+	const float ONE_DIGIT_POS_X = TIME_POS_X + 45.0f;
 	const float TWO_DIGIT_POS_X = TIME_POS_X - 5.0f;
 	const float TIME_NUM_POS_Y = TIME_POS_Y + 40.0f;
-
+	const float TIME_NUM_ORIGIN_X = 28.0f;
+	const float TIME_NUM_ORIGIN_Y = 35.0f;
 
 	float stop_frame = 0.0f;
 	float max_stop   = 2.0f;
