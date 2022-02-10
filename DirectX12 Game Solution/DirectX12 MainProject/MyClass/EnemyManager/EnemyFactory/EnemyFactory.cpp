54d84d6a76014a761b6030fe1494ad6ff5cb7d81
hikaru <opatorishia@gmail.com>
2022-02-10 22:23:44 +0900

    モデルの差し替え

--- DirectX12 Game Solution/DirectX12 MainProject/MyClass/EnemyManager/EnemyFactory/EnemyFactory.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/MyClass/EnemyManager/EnemyFactory/EnemyFactory.cpp
@@ -22,9 +22,9 @@ EnemyFactory::EnemyFactory()
 	enemy_tag.push_back("AR");
 
 	///@“G‚Ìƒ‚ƒfƒ‹@///
-     enemy_model[SWORD_MAN]  = L"Model\\Enemy\\SwordMan\\armor_red_0208.X";
+     enemy_model[SWORD_MAN]  = L"Model\\Enemy\\SwordMan\\armor_red2_0210b.X";
 	 enemy_model[SHIELDER]   = L"Model\\Enemy\\Shielder\\armor_blue_0125.X";
-	 enemy_model[SWORD_MANEX] = L"Model\\Enemy\\SwordMan\\armor_red2_0210.X";
+	 enemy_model[SWORD_MANEX] = L"Model\\Enemy\\SwordManEX\\armor_red2_0210b.X";
 	 enemy_model[ARROW]      = L"Model\\Enemy\\Arrow\\arrow_big.X";
 
 }
